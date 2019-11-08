fd314d66e2b2ffaf55d3f9ab8ee6f195922ec931
MinexAutomation <minex.automation@gmail.com>
2019-11-08 10:42:38 -0800

    Initial commit of source-stub.

---
+++ source/R5T.Lockerbie.Database.Construction/Code/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace R5T.Lockerbie.Database.Construction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ source/R5T.Lockerbie.Database/Code/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace R5T.Lockerbie.Database
+{
+    public class Class1
+    {
+    }
+}
